3a496b597a0185e3f6e7651c80b82c4355b6295a
Yoichiro Tanaka <yoichiro@eisbahn.jp>
2016-04-30 22:00:00 +0900

    Created a Nwetab class.

--- app/scripts.babel/newtab.js
+++ app/scripts.babel/newtab.js
@@ -4,130 +4,140 @@ import MindMap from "./mindmap.js";
 import Parser from "./parser.js";
 import Work from "./work.js";
 
-let mm = new MindMap("#target");
+class Newtab {
 
-let currentWork = Work.newInstance();
+  constructor() {
+    this.mm = new MindMap("#target");
+    this.currentWork = Work.newInstance();
 
-let source = document.querySelector("#source");
+    this.assignEventHandlers();
+    this.loadWorkList();
+  }
 
-source.addEventListener("keydown", e => {
-  var elem, end, start, value;
-  if (e.keyCode === 9) {
-    if (e.preventDefault) {
-      e.preventDefault();
+  assignEventHandlers() {
+    let source = document.querySelector("#source");
+    source.addEventListener("keydown", e => {
+      var elem, end, start, value;
+      if (e.keyCode === 9) {
+        if (e.preventDefault) {
+          e.preventDefault();
+        }
+        elem = e.target;
+        start = elem.selectionStart;
+        end = elem.selectionEnd;
+        value = elem.value;
+        elem.value = "" + value.substring(0, start) + "\t" + value.substring(end);
+        elem.selectionStart = elem.selectionEnd = start + 1;
+        return false;
+      }
+    });
+
+    source.addEventListener("keyup", () => {
+      this.drawMindmap(() => {
+        this.currentWork.save(() => {
+          this.loadWorkList();
+        });
+      });
+    });
+
+    let btnLast = document.querySelector("#btnLast");
+    btnLast.addEventListener("click", () => {
+      Work.getLast(work => {
+        this.load(work);
+      });
+    });
+
+    let btnDelete = document.querySelector("#btnDelete");
+    btnDelete.addEventListener("click", () => {
+      if (this.currentWork.content) {
+        let confirmMessage = document.querySelector("#confirmMessage");
+        confirmMessage.innerText = "Do you really want to delete `" + this.currentWork.firstLine + "`?";
+        $("#confirmDialog").modal("show");
+      }
+    });
+
+    let btnConfirmYes = document.querySelector("#btnConfirmYes");
+    btnConfirmYes.addEventListener("click", () => {
+      $("#confirmDialog").modal("hide");
+      if (this.currentWork.content) {
+        Work.remove(this.currentWork, () => {
+          this.loadWorkList();
+          this.load(Work.newInstance());
+        });
+      }
+    });
+
+    let btnNew = document.querySelector("#btnNew");
+    btnNew.addEventListener("click", () => {
+      this.load(Work.newInstance());
+    });
+
+    let btnTopSites = document.querySelector("#btnTopSites");
+    btnTopSites.addEventListener("click", () => {
+      let text = "Top Sites\n";
+      chrome.topSites.get(sites => {
+        sites.forEach(site => {
+          text = text + "\t[" + site.title + "](" + site.url + ")\n";
+        });
+        let work = new Work(Date.now(), text, Date.now());
+        work.isSave = false;
+        this.load(work);
+      });
+    });
+  }
+
+  drawMindmap(callback) {
+    let source = document.querySelector("#source").value;
+    let root = new Parser().parse(source);
+    if (root) {
+      this.currentWork.content = source;
+      this.mm.draw(root);
+    } else {
+      this.mm.clear();
     }
-    elem = e.target;
-    start = elem.selectionStart;
-    end = elem.selectionEnd;
-    value = elem.value;
-    elem.value = "" + value.substring(0, start) + "\t" + value.substring(end);
-    elem.selectionStart = elem.selectionEnd = start + 1;
-    return false;
+    if (callback) {
+      callback();
+    }
   }
-});
 
-let drawMindmap = (callback) => {
-  let source = document.querySelector("#source").value;
-  let root = new Parser().parse(source);
-  if (root) {
-    currentWork.content = source;
-    mm.draw(root);
-  } else {
-    mm.clear();
+  load(work) {
+    this.currentWork = work;
+    let source = document.querySelector("#source");
+    source.value = this.currentWork.content;
+    this.drawMindmap();
   }
-  if (callback) {
-    callback();
+
+  toLocaleString(date) {
+    return [
+      date.getFullYear(),
+      date.getMonth() + 1,
+      date.getDate()
+    ].join("/") + " " + date.toLocaleTimeString();
   }
+
+  loadWorkList() {
+    Work.getAll(works => {
+      let history = document.querySelector("#history");
+      history.innerHTML = "";
+      works.forEach(work => {
+        let li = document.createElement("li");
+        let link = document.createElement("a");
+        link.href = "#";
+        let label = work.firstLine + " (" + this.toLocaleString(new Date(work.created)) + ")";
+        link.appendChild(document.createTextNode(label));
+        link.addEventListener("click", ((x) => {
+          return () => {
+            this.load(x);
+          };
+        })(work));
+        li.appendChild(link);
+        history.appendChild(li);
+      });
+    });
+  }
+
 }
 
-let load = work => {
-  currentWork = work;
-  let source = document.querySelector("#source");
-  source.value = currentWork.content;
-  drawMindmap();
-};
-
-let toLocaleString = date => {
-  return [
-           date.getFullYear(),
-           date.getMonth() + 1,
-           date.getDate()
-         ].join("/") + " "
-         + date.toLocaleTimeString();
-};
-
-let loadWorkList = () => {
-  Work.getAll(works => {
-    let history = document.querySelector("#history");
-    history.innerHTML = "";
-    works.forEach(work => {
-      let li = document.createElement("li");
-      let link = document.createElement("a");
-      link.href = "#";
-      let label = work.firstLine + " (" + toLocaleString(new Date(work.created)) + ")";
-      link.appendChild(document.createTextNode(label));
-      link.addEventListener("click", ((x) => {
-        return () => {
-          load(x);
-        };
-      })(work));
-      li.appendChild(link);
-      history.appendChild(li);
-    });
-  });
-};
-
-source.addEventListener("keyup", () => {
-  drawMindmap(() => {
-    currentWork.save(() => {
-      loadWorkList();
-    });
-  });
-});
-
-let btnLast = document.querySelector("#btnLast");
-btnLast.addEventListener("click", () => {
-  Work.getLast(work => {
-    load(work);
-  });
-});
-
-loadWorkList();
-
-let btnDelete = document.querySelector("#btnDelete");
-btnDelete.addEventListener("click", () => {
-  if (currentWork.content) {
-    let confirmMessage = document.querySelector("#confirmMessage");
-    confirmMessage.innerText = "Do you really want to delete `" + currentWork.firstLine + "`?";
-    $("#confirmDialog").modal("show");
-  }
-});
-
-let btnConfirmYes = document.querySelector("#btnConfirmYes");
-btnConfirmYes.addEventListener("click", () => {
-  $("#confirmDialog").modal("hide");
-  if (currentWork.content) {
-    Work.remove(currentWork, () => {
-      loadWorkList();
-      load(Work.newInstance());
-    });
-  }
-});
-
-let btnNew = document.querySelector("#btnNew");
-btnNew.addEventListener("click", () => {
-  load(Work.newInstance());
-});
-
-let btnTopSites = document.querySelector("#btnTopSites");
-btnTopSites.addEventListener("click", () => {
-  let text = "Top Sites\n";
-  chrome.topSites.get(sites => {
-    sites.forEach(site => {
-      text = text + "\t[" + site.title + "](" + site.url + ")\n";
-    });
-    let work = new Work(Date.now(), text, Date.now());
-    work.isSave = false;
-    load(work);
-  });
+window.addEventListener("load", () => {
+  new Newtab();
 });
