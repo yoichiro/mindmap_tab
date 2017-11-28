b6ec5c79b0556c0b5951656b74100e11b64d834a
Yoichiro Tanaka <yoichiro.tanaka@58399-MAC.local>
2017-11-28 14:22:53 +0900

    Fix the max length calculation bug.

--- app/scripts.babel/mindmap.js
+++ app/scripts.babel/mindmap.js
@@ -332,9 +332,11 @@ export default class MindMap {
 
   _getAllTextLength(nodes) {
     if (nodes.length > 0) {
-      return this._sum(nodes.map(x => {
+      let sizes = nodes.map(x => {
         return this._measureText(x.text);
-      }));
+      });
+      sizes.push((sizes.length - 1) * NODE_MARGIN_WIDTH);
+      return this._sum(sizes);
     } else {
       return 0;
     }
