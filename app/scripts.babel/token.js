4aa273683f94591668b7393b6f382b6997a03f81
Yoichiro Tanaka <yoichiro@eisbahn.jp>
2017-12-23 21:21:52 +0900

    Add bold/strikethrough text attributes.

--- app/scripts.babel/token.js
+++ app/scripts.babel/token.js
@@ -2,18 +2,32 @@
 
 export default class Token {
 
-  constructor(text, url) {
+  constructor(text, url, bold, strikeThrough) {
     this.text = text;
     this.url = url;
+    this.bold = bold;
+    this.strikeThrough = strikeThrough;
   }
 
   hasUrl() {
     return this.url && this.url.length > 0;
   }
 
+  isBold() {
+    return this.bold;
+  }
+
+  isStrikeThrough() {
+    return this.strikeThrough;
+  }
+
   toHtml() {
     if (this.hasUrl()) {
       return "<a href=\"" + this.url + "\">" + this.escapeHTML(this.text) + "</a>";
+    } else if (this.isBold()) {
+      return "<span><b>" + this.escapeHTML(this.text) + "</b></span>";
+    } else if (this.isStrikeThrough()) {
+      return "<span><s>" + this.escapeHTML(this.text) + "</s></span>";
     } else {
       return "<span>" + this.escapeHTML(this.text) + "</span>";
     }
